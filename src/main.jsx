7f84c46c95c58d80a377c3a46a69555ba301b134
Xenia Ch <eradonna@yandex.ru>
2024-02-13 23:59:28 +0300

    deploument settings

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from './store/index.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/shop-diploma">
         <App />
       </Router>
     </Provider>
